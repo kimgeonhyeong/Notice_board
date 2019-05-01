81fb8eb076504306467e33c50785a6d957472cc2
Kim_Gun <31125535+kimgeonhyeong@users.noreply.github.com>
2019-05-01 10:22:30 +0900

    Update board_write_db.php

--- board_write_db.php
+++ board_write_db.php
@@ -1,17 +1,17 @@
 
         <?php	
 
-			$con = mysqli_connect('localhost','root','apmsetup','notice_board') or die("연결 안됨");
-			mysqli_select_db($con,'notice_board');
-			$Writer =  ($_POST['Writer']);
-			$pass =  ($_POST['pass']);
-            $title =  ($_POST['title']);
-            $comment =  ($_POST['comment']);
-			$date =  date("Y-m-d H:i:s");
-            $visit =  0; 
+	$con = mysqli_connect('localhost','root','apmsetup','notice_board') or die("연결 안됨");
+	mysqli_select_db($con,'notice_board');
+	$Writer =  ($_POST['Writer']);
+	$pass =  ($_POST['pass']);
+	$title =  ($_POST['title']);
+	$comment =  ($_POST['comment']);
+	$date =  date("Y-m-d H:i:s");
+        $visit =  0; 
             //DB 입력
-			$result = mysqli_query($con,"INSERT INTO board_user(board_num, board_title, board_data, board_date, board_visit, board_pass, board_write_user) 
-                                        VALUES('{$board_num}', '{$title}', '{$comment}', '{$date}','{$visit}','{$pass}','{$Writer}');") or die("전송실패");
+		$result = mysqli_query($con,"INSERT INTO board_user(board_num, board_title, board_data, board_date, board_visit, board_pass, board_write_user) 
+                          VALUES('{$board_num}', '{$title}', '{$comment}', '{$date}','{$visit}','{$pass}','{$Writer}');") or die("전송실패");
             mysqli_close($con);
              echo " <script>alert('작성이 완료되었습니다.'); location.href = 'users.php';</script>";
         ?>  
